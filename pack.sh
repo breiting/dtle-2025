8612b388e1d841a501f519d385a750c849f80405
Bernhard Reitinger <bernhard@reitinger.eu>
2025-10-08 08:13:02 +0200

    Initial commit

---
+++ pack.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+read -p "Enter your full name (Firstname Lastname): " fullname
+echo $fullname > NAME
+
+name=$(echo "$fullname" | tr '[:upper:]' '[:lower:]' | tr ' ' '_')
+
+datetime=$(date +"%Y-%m-%d_%H_%M")
+
+filename="${datetime}-${name}.tar.gz"
+
+tar -czf "$filename" ./gtest ./sense ./tests CMakeLists.txt
+
+echo "Package created: $filename"
+echo "Please now upload this file to Moodle!"
